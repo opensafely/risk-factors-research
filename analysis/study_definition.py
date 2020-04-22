7cc20b517592d32bf399ed02ff8eac001ace6ef1
Alex Walker <alex.walker@thedatalab.org>
2020-04-22 18:04:07 +0100

    Add smoking to study definition, but commented out

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -11,6 +11,9 @@ stroke = codelist_from_csv(
 
 dementia = codelist_from_csv(
     "codelists/dementia.csv", system="ctv3", column="CTV3ID")
+smoking_codes  = codelist_from_csv(
+    "codelists/smoking_codes_2020_04_22.csv", system="ctv3", column="CTV3Code", category_column="Category"
+)
 
 other_neuro = codelist_from_csv(
     "codelists/other_neuro.csv", system="ctv3", column="CTV3ID")
@@ -154,6 +157,28 @@ study = StudyDefinition(
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/6
     #smoking_status= # still to be implemented
 
+    # smoking_status=patients.categorised_as(
+    #     {
+    #         "S": "most_recent_smoking_code = 'S'",
+    #         "E": """
+    #              most_recent_smoking_code = 'E' OR (
+    #                most_recent_smoking_code = 'N' AND ever_smoked
+    #              )
+    #         """,
+    #         "N": "most_recent_smoking_code = 'N' AND NOT ever_smoked",
+    #         "M": "DEFAULT"
+    #     },
+    #     most_recent_smoking_code=patients.with_these_clinical_events(
+    #         smoking_codes,
+    #         find_last_match_in_period=True,
+    #         on_or_before='2020-02-01',
+    #         returning="category",
+    #     ),
+    #     ever_smoked=patients.with_these_clinical_events(
+    #         filter_codes_by_category(smoking_codes, include=['S', 'E']),
+    #         on_or_before='2020-02-01'
+    #     ),
+    # ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/27
     #ethnicity= # still to be implemented - this will be just the Read code for now then can be categorised with the list we're making.
     ethnicity=patients.with_these_clinical_events(
